125957a2029b346b6c84a545cd19bf33c969247d
Eduardo Fernandes <eduardo.fernandes@matchboxbrasil.com>
2020-11-11 17:49:49 -0300

    Add cors

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./src/routes");
 require("dotenv").config();
+const cors = require("cors");
 
+app.use(cors());
 const app = express();
 mongoose.connect(process.env.database, {
   useNewUrlParser: true,
